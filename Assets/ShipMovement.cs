3a91aff8cf637d7d90c5c87b6d5821a2b9e14600
Blake Erquiaga <cbcerquiaga@me.com>
2020-10-20 12:38:17 -0400

    Added pseudocode for ship movement
    #1 Added pseudocode detailing how to implement ship movement for a single ship

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        boolean isPaused = true
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if !isPaused{
+          //apply directional forces from movement plan
+          //check sail impact on speed
+            //sail area
+            //sail angle vs star
+            //blocked?
+          //check damage impact on movements
+
+        } else {
+          //let the player change directional forces
+          //engine acceleration
+          //roll rotation
+          //pitch rotation
+          //yaw rotation
+          //sail area [if the ship has a sail]
+          //let players delay certain actionsfor later...queue of movementElement objects? Movement phases?
+
+        }
+    }
+}
