d02fe11b52be752b003812b9e98d0ec46b6e642f
Blake Erquiaga <cbcerquiaga@me.com>
2020-10-21 11:52:55 -0400

    Started adding real code
    #1

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+
+  public float movespeed = 10f;
+  public float currentSpeed = 10f;
+  public float turnspeed = 50f;
+  public float rollspeed = 50f;
+  public float pitchspeed = 50f;
+  public float accelRate = 10f;
+  public float maxSpeedDownWind = 10f;
+  public float maxSpeedCrossWind = 10f;
+  public float maxSpeedUpWind = 10f;
+  public float maxSpeedNoWind = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        boolean isPaused = true
+        boolean isPaused = true;
+
+        //queue of movementActions? time limit of maneuvers?
     }
 
     // Update is called once per frame
     void Update()
     {
         if !isPaused{
-          //apply directional forces from movement plan
+          //apply directional forces from movement plan in order
           //check sail impact on speed
             //sail area
             //sail angle vs star
@@ -24,12 +37,13 @@ public class ShipMovement : MonoBehaviour
         } else {
           //let the player change directional forces
           //engine acceleration
+          if(Input.GetKey(KeyCode.UpArrow))
+             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
           //roll rotation
           //pitch rotation
           //yaw rotation
           //sail area [if the ship has a sail]
-          //let players delay certain actionsfor later...queue of movementElement objects? Movement phases?
-
+          //add action to queue
         }
     }
 }
