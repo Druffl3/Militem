a139868ace11697bc1b2796c7f4d8b9c0c48c71e
Blake Erquiaga <cbcerquiaga@me.com>
2020-10-25 07:49:59 -0400

    Added ShipMovementElement Object

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
 
-  public float currentSpeed = 0f; //"current" movement speed when game unpauses
-  public float acceleration = 0f; //current change in velocity
-  public float yawspeed = 30f; //rate of yaw
-  public float rollspeed = 40f; //rate of roll
-  public float pitchspeed = 50f; //rate of pitch
-  public float accelRate = 0.1f; //rate of acceleration
-  public float maxAcceleration = 10f; //maximum acceleration, minimum is negative of this
-  public float maxSpeedDownWind = 10f;
-  public float maxSpeedCrossWind = 10f;
-  public float maxSpeedUpWind = 10f;
-  public float maxSpeedNoWind = 10f;
+  //TODO: add these to the ship classes themselves
+  // public float currentSpeed = 0; //"current" movement speed when game unpauses
+  // public float acceleration = 0; //current change in velocity
+  // public float yawspeed = 30; //rate of yaw
+  // public float rollspeed = 40; //rate of roll
+  // public float pitchspeed = 50; //rate of pitch
+  // public float accelRate = 0.1; //rate of acceleration
+  // public float maxAcceleration = 10; //maximum acceleration, minimum is negative of this
+  // public float maxSpeedDownWind = 100;
+  // public float maxSpeedCrossWind = 80;
+  // public float maxSpeedUpWind = 60;
+  // public float maxSpeedNoWind = 75;
+  // public float fuelLevel = 100;
+
+  //maybe make a different object type for this or store it as something else?
+  public ShipMovementElement originalState; //the state the ship is in before the turn
+
+  public ShipMovementElement[] movementArray;
     // Start is called before the first frame update
     void Start()
     {
         boolean isPaused = true;
 
-        //queue of movementActions?
+        timer = Stopwatch.new();
     }
 
     // Update is called once per frame
@@ -36,6 +43,8 @@ public class ShipMovement : MonoBehaviour
             //blocked?
           //check damage impact on movements
         } else {
+          ShipMovementElement tempElement = new ShipMovementElement();
+          timer.Start();
           //let the player change directional forces
           //engine acceleration
           if(Input.GetKey(KeyCode.UpArrow)){
@@ -53,6 +62,9 @@ public class ShipMovement : MonoBehaviour
           //yaw rotation
           //sail area [if the ship has a sail]
           //add action to queue
+          timer.Stop();
+          tempElement.milliseconds = timer.ElapsedMilliseconds;
+
         }
     }
 
---
+++ Assets/ShipMovementElement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This is to store input data from player flight movements
+These will be stored in an array in order to replay all of the movements at once
+*/
+
+public class ShipMovementElement : MonoBehaviour
+{
+    public int x_pos;
+    public int y_pos;
+    public int z_pos;
+    public float x_rot;
+    public float y_rot;
+    public float z_rot;
+    public float sailState;
+    public float fuelLevel;
+    public int milliseconds; //number of milliseconds since the previous action
+
+}
