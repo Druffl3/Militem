70268fd6f57eb183b230803b2a53c5b1d3cdb274
Blake Erquiaga <cbcerquiaga@me.com>
2020-10-25 08:20:52 -0400

    Attempted to add Acceleration
    #1 running into compiling problems though

---
+++ Assets/FlightControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightControl : MonoBehaviour
+{
+  public float currentSpeed = 0; //"current" movement speed when game unpauses
+  public float acceleration = 0; //current change in velocity
+  public float yawspeed = 30; //rate of yaw
+  public float rollspeed = 40; //rate of roll
+  public float pitchspeed = 50; //rate of pitch
+  public float accelRate = 0.1; //rate of acceleration
+  public float maxAcceleration = 10; //maximum acceleration, minimum is negative of this
+  public float maxSpeedDownWind = 1000;
+  public float maxSpeedCrossWind = 800;
+  public float maxSpeedUpWind = 600;
+  public float maxSpeedNoWind = 750;
+  public float fuelLevel = 100;
+    void Start() {}
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(Keycode.Return)){
+        acceleration = acceleration + accelRate;
+        if (acceleration > maxAcceleration){
+          acceleration = maxAcceleration;
+        }
+
+        currentSpeed = currentSpeed + acceleration;
+        if (currentSpeed > maxSpeedNoWind){
+          currentSpeed = maxSpeedNoWind;
+        }
+      } else if (Input.GetKeyUp(KeyKode.Return)){
+        acceleration = 0;
+      }
+
+      transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+    }
+}
--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -5,20 +5,6 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
 
-  //TODO: add these to the ship classes themselves
-  // public float currentSpeed = 0; //"current" movement speed when game unpauses
-  // public float acceleration = 0; //current change in velocity
-  // public float yawspeed = 30; //rate of yaw
-  // public float rollspeed = 40; //rate of roll
-  // public float pitchspeed = 50; //rate of pitch
-  // public float accelRate = 0.1; //rate of acceleration
-  // public float maxAcceleration = 10; //maximum acceleration, minimum is negative of this
-  // public float maxSpeedDownWind = 100;
-  // public float maxSpeedCrossWind = 80;
-  // public float maxSpeedUpWind = 60;
-  // public float maxSpeedNoWind = 75;
-  // public float fuelLevel = 100;
-
   //maybe make a different object type for this or store it as something else?
   public ShipMovementElement originalState; //the state the ship is in before the turn
 
@@ -47,21 +33,11 @@ public class ShipMovement : MonoBehaviour
           timer.Start();
           //let the player change directional forces
           //engine acceleration
-          if(Input.GetKey(KeyCode.UpArrow)){
-            acceleration = acceleration + accelRate;
-            if acceleration > maxAcceleration{
-              acceleration = maxAcceleration;
-            } else if acceleration < (0 - maxAcceleration){
-              acceleration = 0 - maxAcceleration;
-            }
-            currentSpeed = currentSpeed + acceleration;
-          }
-
           //roll rotation
           //pitch rotation
           //yaw rotation
           //sail area [if the ship has a sail]
-          //add action to queue
+          //add action to array
           timer.Stop();
           tempElement.milliseconds = timer.ElapsedMilliseconds;
 
@@ -70,6 +46,6 @@ public class ShipMovement : MonoBehaviour
 
     //apply movement inputs to the ship
     void applyMoves(){
-      transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+      //take movementElements from the array and move the ship to those positions
     }
 }
