0510b485ecfbdb84f68c6eadabcd435b50dc7fab
Blake Erquiaga <cbcerquiaga@me.com>
2020-10-28 15:27:12 -0400

    Cylinder moves
    closed #1 it's extremely slow though, not sure why

--- Assets/FlightControl.cs
+++ Assets/FlightControl.cs
@@ -9,32 +9,32 @@ public class FlightControl : MonoBehaviour
   public float yawspeed = 30; //rate of yaw
   public float rollspeed = 40; //rate of roll
   public float pitchspeed = 50; //rate of pitch
-  public float accelRate = 0.1; //rate of acceleration
-  public float maxAcceleration = 10; //maximum acceleration, minimum is negative of this
-  public float maxSpeedDownWind = 1000;
-  public float maxSpeedCrossWind = 800;
-  public float maxSpeedUpWind = 600;
-  public float maxSpeedNoWind = 750;
+  public float accelRate = 100f; //rate of acceleration
+  public float maxAcceleration = 1000; //maximum acceleration, minimum is negative of this
+  public float maxSpeedDownWind = 10000;
+  public float maxSpeedCrossWind = 8000;
+  public float maxSpeedUpWind = 6000;
+  public float maxSpeedNoWind = 7500;
   public float fuelLevel = 100;
     void Start() {}
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(Keycode.Return)){
+      if (Input.GetKeyDown(KeyCode.Return)){
         acceleration = acceleration + accelRate;
         if (acceleration > maxAcceleration){
           acceleration = maxAcceleration;
         }
-
         currentSpeed = currentSpeed + acceleration;
-        if (currentSpeed > maxSpeedNoWind){
-          currentSpeed = maxSpeedNoWind;
-        }
-      } else if (Input.GetKeyUp(KeyKode.Return)){
+      } else if (Input.GetKeyUp(KeyCode.Return)){
         acceleration = 0;
       }
 
-      transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+      if (currentSpeed > maxSpeedNoWind){
+        currentSpeed = maxSpeedNoWind;
+      }
+
+      transform.Translate(Vector3.up * currentSpeed * Time.deltaTime);
     }
 }
--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -1,51 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShipMovement : MonoBehaviour
-{
-
-  //maybe make a different object type for this or store it as something else?
-  public ShipMovementElement originalState; //the state the ship is in before the turn
-
-  public ShipMovementElement[] movementArray;
-    // Start is called before the first frame update
-    void Start()
-    {
-        boolean isPaused = true;
-
-        timer = Stopwatch.new();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if !isPaused{
-          //apply directional forces from movement plan in order
-          applyMoves()
-          //check sail impact on speed
-            //sail area
-            //sail angle vs star
-            //blocked?
-          //check damage impact on movements
-        } else {
-          ShipMovementElement tempElement = new ShipMovementElement();
-          timer.Start();
-          //let the player change directional forces
-          //engine acceleration
-          //roll rotation
-          //pitch rotation
-          //yaw rotation
-          //sail area [if the ship has a sail]
-          //add action to array
-          timer.Stop();
-          tempElement.milliseconds = timer.ElapsedMilliseconds;
-
-        }
-    }
-
-    //apply movement inputs to the ship
-    void applyMoves(){
-      //take movementElements from the array and move the ship to those positions
-    }
-}
+// ﻿using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// public class ShipMovement : MonoBehaviour
+// {
+//
+//   //maybe make a different object type for this or store it as something else?
+//   public ShipMovementElement originalState; //the state the ship is in before the turn
+//
+//   public ShipMovementElement[] movementArray;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         boolean isPaused = true;
+//
+//         timer = Stopwatch.new();
+//     }
+//
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if !isPaused{
+//           //apply directional forces from movement plan in order
+//           applyMoves()
+//           //check sail impact on speed
+//             //sail area
+//             //sail angle vs star
+//             //blocked?
+//           //check damage impact on movements
+//         } else {
+//           ShipMovementElement tempElement = new ShipMovementElement();
+//           timer.Start();
+//           //let the player change directional forces
+//           //engine acceleration
+//           //roll rotation
+//           //pitch rotation
+//           //yaw rotation
+//           //sail area [if the ship has a sail]
+//           //add action to array
+//           timer.Stop();
+//           tempElement.milliseconds = timer.ElapsedMilliseconds;
+//
+//         }
+//     }
+//
+//     //apply movement inputs to the ship
+//     void applyMoves(){
+//       //take movementElements from the array and move the ship to those positions
+//     }
+// }
