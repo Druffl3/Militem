05e372e88112cf049bcf63147cd6e9ac9ebb0694
Blake Erquiaga <cbcerquiaga@me.com>
2020-10-28 15:30:42 -0400

    Scene change
    There is a cube for reference to know if the cylinder is moving properly

--- Assets/FlightControl.cs
+++ Assets/FlightControl.cs
@@ -9,12 +9,12 @@ public class FlightControl : MonoBehaviour
   public float yawspeed = 30; //rate of yaw
   public float rollspeed = 40; //rate of roll
   public float pitchspeed = 50; //rate of pitch
-  public float accelRate = 100f; //rate of acceleration
-  public float maxAcceleration = 1000; //maximum acceleration, minimum is negative of this
-  public float maxSpeedDownWind = 10000;
-  public float maxSpeedCrossWind = 8000;
-  public float maxSpeedUpWind = 6000;
-  public float maxSpeedNoWind = 7500;
+  public float accelRate = 100000000f; //rate of acceleration
+  public float maxAcceleration = 10000000000f; //maximum acceleration, minimum is negative of this
+  public float maxSpeedDownWind = 100000f;
+  public float maxSpeedCrossWind = 80000f;
+  public float maxSpeedUpWind = 60000f;
+  public float maxSpeedNoWind = 7500000000f;
   public float fuelLevel = 100;
     void Start() {}
 
